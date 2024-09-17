bfffb4b73c41ea1b248358dfa22ea48007501dfe
Adauto Moisés <adautomaleandro@gmail.com>
2024-09-17 13:41:17 -0300

    commit inicial

---
+++ my-project/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/App.kt
@@ -0,0 +1,8 @@
+package tech.salvatore.livro_android_kotlin_paulo_salvatore
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
